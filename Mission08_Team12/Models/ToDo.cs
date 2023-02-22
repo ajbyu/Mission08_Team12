8a33d1e8f72379a213d475591979d349dd198b86
nbain00 <zink47@gmail.com>
2023-02-22 13:36:00 -0700

    added models and database config

---
+++ Mission08_Team12/Models/ToDo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission08_Team12.Models
+{
+    public class ToDo
+    {
+        [Key]
+        public int ToDoID { get; set; }
+        [Required]
+        public string Task { get; set; }
+        public string DueDate { get; set; }
+        [Required]
+        public int Quadrant { get; set; }
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+        public bool Completed { get; set; }
+    }
+}
